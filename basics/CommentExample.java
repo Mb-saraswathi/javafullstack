f9b0ae0edf6dc3ee1aa4bac6e5ea5cd1c0e2d7d1
Mb-saraswathi <hopesoul321@gmail.com>
2025-06-17 07:44:59 +0530

    Update CommentExample.java

--- basics/CommentExample.java
+++ basics/CommentExample.java
@@ -1,15 +1,9 @@
-// This is a single-line comment
 
-/*
-This is a
-multi-line comment
-*/
-
-/**
- * This is a documentation comment
- */
-public class CommentExample {
-    public static void main(String[] args) {
-        System.out.println("Comments in Java");
-    }
+public class CommentExample
+{
+public static void main(String[] args)
+ {
+    
+System.out.println("Comments in Java");
+ }
 }
